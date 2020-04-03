47a7df3467d968c182cdfdc795e9a52a21a13f98
Grace Looney <gralloon@ut.utm.edu>
2020-04-03 13:26:37 -0500

    Created a timer on New Game

--- Maze/NewGame.xaml.cs
+++ Maze/NewGame.xaml.cs
@@ -11,17 +11,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+//Timer code was adapted from https://stackoverflow.com/questions/8302590/running-stopwatch-in-textblock
 namespace Maze
 {
     /// <summary>
     /// Interaction logic for NewGame.xaml
     /// </summary>
-    public partial class NewGame : Window
+    public partial class NewGame : Window, INotifyPropertyChanged
     {
         public NewGame()
         {
             InitializeComponent();
+            this.Stopwatch = new Stopwatch();
+            this.Stopwatch.Start();
+            this._timer = new Timer(
+                new TimerCallback((s) => this.FirePropertyChanged(this, new PropertyChangedEventArgs("Stopwatch"))),
+                null, 1000, 1000);
+            Game n = new Game();
+        }
+        private void FirePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(sender, e);
+            }
+        }
+
+        private Timer _timer;
+
+        public Stopwatch Stopwatch { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
