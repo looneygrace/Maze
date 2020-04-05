234b4e16623ace03ad8b7788d6cb97208a1fe0d4
Grace Looney <gralloon@ut.utm.edu>
2020-04-04 21:04:51 -0500

    Settings is accepting number input but box is not updating the color

--- Maze/Settings.xaml.cs
+++ Maze/Settings.xaml.cs
@@ -22,6 +22,46 @@ namespace Maze
         public Settings()
         {
             InitializeComponent();
+
+            ColorBox.Fill = bConverter();
+        }
+
+        public Brush bConverter()
+        {
+            string r = Red.Text;
+            string b = Blue.Text;
+            string g = Green.Text;
+
+            int redInt = int.Parse(r);
+            int greenInt = int.Parse(g);
+            int blueInt = int.Parse(b);
+            if(redInt >= 255)
+            {
+                redInt = 255;
+                Red.Text = "255";
+            }
+            if (blueInt >= 255)
+            {
+                blueInt = 255;
+                Blue.Text = "255";
+            }
+            if (greenInt >= 255)
+            {
+               greenInt = 255;
+               Green.Text = "255";
+            }
+            if(redInt <= 0)
+            {
+                redInt = 0;
+                Red.Text = "0";
+            }
+            byte red = Convert.ToByte(redInt);
+            byte blue = Convert.ToByte(redInt);
+            byte green = Convert.ToByte(redInt);
+
+            SolidColorBrush brush = new SolidColorBrush();
+            brush.Color = Color.FromRgb(red, green, blue);
+            return brush;
         }
 
         private void Home_Click(object sender, RoutedEventArgs e)
