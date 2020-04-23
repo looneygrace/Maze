3d36a73377a08104f4af8c48f3a933cd074e2131
looneygrace1 <gralloon@ut.utm.edu>
2020-04-22 23:39:12 -0500

    Compiles

--- Maze/MainWindow.xaml.cs
+++ Maze/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,7 @@ namespace Maze
         public MainWindow()
         {
             InitializeComponent();
+            customSettings();
         }
 
         private void New_Game_Click(object sender, RoutedEventArgs e)
@@ -58,6 +60,45 @@ namespace Maze
             var Settings = new Settings();
             Settings.Show();
             this.Close();
+        }
+        private void customSettings()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines("../../Settings.txt");
+
+            if (lines[0] == "../../CustomSettings.txt")
+            {
+                //custom = true;
+                string[] customlines = System.IO.File.ReadAllLines("../../CustomSettings.txt");
+                Console.Write("CUSTOM");
+                SolidColorBrush FloorColor = new SolidColorBrush();
+                SolidColorBrush WallColor = new SolidColorBrush();
+                WallColor.Color = Color.FromRgb(Convert.ToByte(customlines[0]), Convert.ToByte(customlines[2]), Convert.ToByte(customlines[3]));
+                FloorColor.Color = Color.FromRgb(Convert.ToByte(customlines[3]), Convert.ToByte(customlines[5]), Convert.ToByte(customlines[4]));
+
+                this.Background = WallColor;
+                //fl.Fill = FloorColor;
+
+            }
+            else
+            {
+                ImageBrush myBrush = new ImageBrush();
+                myBrush.ImageSource =
+                    new BitmapImage(new Uri(lines[0], UriKind.Relative));
+                this.Background = myBrush;
+                //fl.Opacity = .35;
+            }
+
+            MediaPlayer sound;
+            var uri = new Uri(lines[1], UriKind.Relative);
+
+            sound = new MediaPlayer();
+
+            sound.Open(uri);
+
+            sound.Play();
+            SystemSounds.Beep.Play();
+            Console.WriteLine(lines[1]);
         }
     }
 }
